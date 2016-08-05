ae75cf88cb235ca90d9763ea3aa220c107fd209a
Ryan Kennedy <kndyry@users.noreply.github.com>
2016-08-05 17:11:36 -0400

    make flags agnostic, add port args for both hosts
    rename (prod, dev) based flags to agnostic (a, b) convention. add -M arg for host b mysql port.

--- wot-dbsync.sh
+++ wot-dbsync.sh
@@ -7,17 +7,18 @@
 #   -T is overwritten with the saved export of -t.
 #
 # USAGE ===========================================================
-#   $ ./wot-dbsync sync -m port -t db -h host -u user -p pass -T db -H host -U user -P pass -e export/path
-#   where -m = the mysql port
-#         -t = the production server target db
-#         -h = the production server's address
-#         -u = the production mysql username
-#         -p = the production mysql password
-#         -T = the development server target db
-#         -H = the development server's address
-#         -U = the development mysql username
-#         -P = the development mysql password
-#         -e = the location to store db exports (no trailing slash)
+#   $ ./wot-dbsync sync -m port -d db -h host -u user -p pass -M port -D db -H host -U user -P pass -o export/path
+#   where -m = server a's mysql port
+#         -d = server a's target db
+#         -h = server a's address
+#         -u = server a's mysql username
+#         -p = server a's mysql password
+#         -M = server b's mysql port
+#         -D = server b's target db
+#         -H = server b's address
+#         -U = server b's mysql username
+#         -P = server b's mysql password
+#         -o = location to store exports (no trailing slash)
 #
 # Created: 05-Aug-2016
 
@@ -39,57 +40,59 @@ timestamp () {
 
 do_sync () {
   stamp=$(timestamp)
-  pfile=${prod}_${stamp}_${ptarg}.sql
-  dfile=${dev}_${stamp}_${dtarg}.sql
+  afile=${ahost}_${stamp}_${atarg}.sql
+  bfile=${bhost}_${stamp}_${btarg}.sql
 
   # Export
-  mysqldump --add-drop-table -P ${port} -h ${prod} -u ${puser} -p${ppass} -r ${path}/${pfile} ${ptarg} 2>/dev/null
-  mysqldump --add-drop-table -P ${port} -h ${dev} -u ${duser} -p${dpass} -r ${path}/${dfile} ${dtarg} 2>/dev/null
+  mysqldump --add-drop-table -P ${aport} -h ${ahost} -u ${auser} -p${apass} -r ${path}/${afile} ${atarg} 2>/dev/null
+  mysqldump --add-drop-table -P ${bport} -h ${bhost} -u ${buser} -p${bpass} -r ${path}/${bfile} ${btarg} 2>/dev/null
 
   # Import
-  if [[ -e ${path}/${pfile} && -s ${path}/${pfile} ]] ; then
-    if [[ -e ${path}/${dfile} && -s ${path}/${dfile} ]] ; then
-      mysql -P ${port} -h ${dev} -u ${duser} -p${dpass} ${dtarg} < ${path}/${pfile} 2>/dev/null
-      success "synced ${prod}/${ptarg} to ${dev}/${dtarg} using ${path}/${pfile}..."
+  if [[ -e ${path}/${afile} && -s ${path}/${afile} ]] ; then
+    if [[ -e ${path}/${bfile} && -s ${path}/${bfile} ]] ; then
+      mysql -P ${bport} -h ${bhost} -u ${buser} -p${bpass} ${btarg} < ${path}/${afile} 2>/dev/null
+      success "synced ${ahost}/${atarg} to ${bhost}/${btarg} using ${path}/${afile}..."
     else
-      fail "${dev} database incomplete or unavailable!"
+      fail "${bhost} database incomplete or unavailable!"
     fi
   else
-    fail "${prod} database incomplete or unavailable!"
+    fail "${ahost} database incomplete or unavailable!"
   fi
 }
 
 if [[ ${1} == 'sync' ]] ; then
   shift
-  args=$(getopt m:t:h:u:p:T:H:U:P:e: $*)
+  args=$(getopt m:d:h:u:p:M:D:H:U:P:o: $*)
   set -- $args
   for i ; do
     case "$i" in
-      -m ) port="${2}"
+      -m ) aport="${2}"
            shift ; shift ;;
-      -t ) ptarg="${2}"
+      -d ) atarg="${2}"
            shift ; shift ;;
-      -h ) prod="${2}"
+      -h ) ahost="${2}"
            shift ; shift ;;
-      -u ) puser="${2}"
+      -u ) auser="${2}"
            shift ; shift ;;
-      -p ) ppass="${2}"
+      -p ) apass="${2}"
            shift ; shift ;;
-      -T ) dtarg="${2}"
+      -M ) bport="${2}"
            shift ; shift ;;
-      -H ) dev="${2}"
+      -D ) btarg="${2}"
            shift ; shift ;;
-      -U ) duser="${2}"
+      -H ) bhost="${2}"
            shift ; shift ;;
-      -P ) dpass="${2}"
+      -U ) buser="${2}"
            shift ; shift ;;
-      -e ) path="${2}"
+      -P ) bpass="${2}"
+           shift ; shift ;;
+      -o ) path="${2}"
            shift ; shift ;;
       -- ) shift ; break ;;
     esac
   done
-  if [[ ! -z ${port} && ! -z ${ptarg} && ! -z ${prod} && ! -z ${puser} && ! -z ${ppass} && ! -z ${dtarg} && ! -z ${dev} && ! -z ${duser} && ! -z ${dpass} && ! -z ${path} ]] ; then
-    do_sync ${port} ${ptarg} ${prod} ${puser} ${ppass} ${dtarg} ${dev} ${duser} ${dpass} ${path}
+  if [[ ! -z ${aport} && ! -z ${atarg} && ! -z ${ahost} && ! -z ${auser} && ! -z ${apass} && ! -z ${bport} && ! -z ${btarg} && ! -z ${bhost} && ! -z ${buser} && ! -z ${bpass} && ! -z ${path} ]] ; then
+    do_sync ${aport} ${atarg} ${ahost} ${auser} ${apass} ${bport} ${btarg} ${bhost} ${buser} ${bpass} ${path}
   else
     fail "invalid or missing arguments!"
   fi

e48e9dafcfd81828871463ea7b7065b68f4f43e0
kndyry <ry@nkennedy.net>
2016-08-06 10:37:35 -0400

    add simultaneous ssh

--- wot-dbsync.sh
+++ wot-dbsync.sh
@@ -2,22 +2,22 @@
 #
 # WHAT ============================================================
 #   Connects to MySQL over ports -m and -M on servers -h and -H,
-#   exports databases -d and -D, then saves the files at -e.
-#   If files for -d and -D exist at -e and are non-zero, -D is
+#   exports databases -d and -D, then saves the files at -o.
+#   If files for -d and -D exist at -o and are non-zero, -D is
 #   overwritten with the saved export of -d. Optional invocations
 #   can be used to connect to -h or -H over SSH, or to save exports
-#   of -d and -D at -e without -D being overwritten.
+#   of -d and -D at -o without -D being overwritten.
 #
 # USAGE ===========================================================
-#   $ ./wot-dbsync sync -s host -S user -m port -d db -h host -u user -p pass -M port -D db -H host -U user -P pass -o export/path
+#   $ ./wot-dbsync sync -s user@host -m port -d db -h host -u user -p pass -S user@host -M port -D db -H host -U user -P pass -o export/path
 #   ( ) = optional, (*) = required:
-#   where -s = ( ) for host -h or -H, connect over ssh
-#         -S = ( ) if -s is set, login as user -S
+#   where -s = ( ) when connecting to source, use ssh string -s
 #         -m = (*) connect to mysql over port -m on source
 #         -d = (*) export database -d from source
 #         -h = (*) connect to mysql host -h on source
 #         -u = (*) login as user -u on source
 #         -p = (*) login with password -p on source
+#         -S = ( ) when connecting to destination, use ssh string -s
 #         -M = (*) connect to mysql over port -M on destination
 #         -D = (*) export database -D from destination
 #         -H = (*) connect to mysql host -H on destination
@@ -47,8 +47,8 @@ timestamp () {
 }
 
 export_ssh () {
-  # suser, shost, port, host, user, pass, data, path, file
-  ssh ${1}@${2} "mysqldump --add-drop-table -P ${3} -h ${4} -u ${5} -p${6} ${7}" > ${8}/${9}
+  # ssh, port, host, user, pass, data, path, file
+  ssh ${1} "mysqldump --add-drop-table -P ${2} -h ${3} -u ${4} -p${5} ${6}" > ${7}/${8}
 }
 
 export_mysql () {
@@ -57,8 +57,8 @@ export_mysql () {
 }
 
 import_ssh () {
-  # suser, shost, port, host, user, pass, data, path, file
-  ssh ${1}@${2} "mysql -P ${3} -h ${4} -u ${5} -p${6} ${7}" < ${8}/${9}
+  # ssh, port, host, user, pass, data, path, file
+  ssh ${1} "mysql -P ${2} -h ${3} -u ${4} -p${5} ${6}" < ${7}/${8}
 }
 
 import_mysql () {
@@ -72,22 +72,15 @@ do_sync () {
   bfile=${bhost}_${stamp}_${bdata}.sql
 
   # Export
-  if [[ ! -z ${shost} ]] ; then
-    if [[ ! -z ${suser} ]] ; then
-      if [[ ${shost} == ${ahost} ]] ; then
-        export_ssh ${suser} ${shost} ${aport} ${ahost} ${auser} ${apass} ${adata} ${path} ${afile}
-        export_mysql ${bport} ${bhost} ${buser} ${bpass} ${path} ${bfile} ${bdata}
-      elif [[ ${shost} == ${bhost} ]] ; then
-        export_mysql ${aport} ${ahost} ${auser} ${apass} ${path} ${afile} ${adata}
-        export_ssh ${suser} ${shost} ${bport} ${bhost} ${buser} ${bpass} ${bdata} ${path} ${bfile}
-      else
-        fail "invalid ssh hostname, must match -h or -H"
-      fi
-    else
-      fail "invalid or missing ssh arguments"
-    fi
+  if [[ ! -z ${sshca} ]] ; then
+    export_ssh ${sshca} ${aport} ${ahost} ${auser} ${apass} ${adata} ${path} ${afile}
   else
     export_mysql ${aport} ${ahost} ${auser} ${apass} ${path} ${afile} ${adata}
+  fi
+
+  if [[ ! -z ${sshcb} ]] ; then
+    export_ssh ${sshcb} ${bport} ${bhost} ${buser} ${bpass} ${bdata} ${path} ${bfile}
+  else
     export_mysql ${bport} ${bhost} ${buser} ${bpass} ${path} ${bfile} ${bdata}
   fi
 
@@ -95,8 +88,8 @@ do_sync () {
   if [[ -e ${path}/${afile} && -s ${path}/${afile} ]] ; then
     if [[ -e ${path}/${bfile} && -s ${path}/${bfile} ]] ; then
       if [[ ${action} == "sync" ]] ; then
-        if [[ ${shost} == ${bhost} ]] ; then
-          import_ssh ${suser} ${shost} ${bport} ${bhost} ${buser} ${bpass} ${bdata} ${path} ${afile}
+        if [[ ! -z ${sshcb} ]] ; then
+          import_ssh ${sshcb} ${bport} ${bhost} ${buser} ${bpass} ${bdata} ${path} ${afile}
         else
           import_mysql ${bport} ${bhost} ${buser} ${bpass} ${bdata} ${path} ${afile}
         fi
@@ -115,13 +108,11 @@ do_sync () {
 if [[ ${1} == "sync" || ${1} == "backup" ]] ; then
   action=${1}
   shift
-  args=$(getopt s:S:m:d:h:u:p:M:D:H:U:P:o: $*)
+  args=$(getopt s:m:d:h:u:p:S:M:D:H:U:P:o: $*)
   set -- $args
   for i ; do
     case "$i" in
-      -s ) shost="${2}"
-           shift ; shift ;;
-      -S ) suser="${2}"
+      -s ) sshca="${2}"
            shift ; shift ;;
       -m ) aport="${2}"
            shift ; shift ;;
@@ -132,6 +123,8 @@ if [[ ${1} == "sync" || ${1} == "backup" ]] ; then
       -u ) auser="${2}"
            shift ; shift ;;
       -p ) apass="${2}"
+           shift ; shift ;;
+      -S ) sshcb="${2}"
            shift ; shift ;;
       -M ) bport="${2}"
            shift ; shift ;;
@@ -149,7 +142,7 @@ if [[ ${1} == "sync" || ${1} == "backup" ]] ; then
     esac
   done
   if [[ ! -z ${aport} && ! -z ${adata} && ! -z ${ahost} && ! -z ${auser} && ! -z ${apass} && ! -z ${bport} && ! -z ${bdata} && ! -z ${bhost} && ! -z ${buser} && ! -z ${bpass} && ! -z ${path} ]] ; then
-    do_sync ${action} ${shost} ${suser} ${aport} ${adata} ${ahost} ${auser} ${apass} ${bport} ${bdata} ${bhost} ${buser} ${bpass} ${path}
+    do_sync ${action} ${sshca} ${aport} ${adata} ${ahost} ${auser} ${apass} ${sshcb} ${bport} ${bdata} ${bhost} ${buser} ${bpass} ${path}
   else
     fail "invalid or missing arguments"
   fi
